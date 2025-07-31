3735022abd7c9ffc186fe3ad4c4c004a25769761
3rick5h <ericksh2009@gmail.com>
2025-07-31 11:15:04 -0600

    Add files via upload

---
+++ CURSO JAVASCRIPT/02-operadores/01-aritmeticos.js
@@ -0,0 +1,18 @@
+let a = 5;
+let b = 7;
+
+// console.log(a + b, 'adicion'); //adicion 
+// console.log(a - b, 'resta'); //resta
+// console.log(a * b, 'multiplicación'); // muliplicación
+// console.log(a / b, 'división'); //división
+// console.log(a % b, 'modulo'); // modulo
+// console.log(a ** b, 'potencia'); // potencia
+
+//incrementar
+console.log(++a);
+console.log(a++);
+console.log(a);
+//decrementar
+console.log(--a);
+console.log(a--);
+console.log(a);
